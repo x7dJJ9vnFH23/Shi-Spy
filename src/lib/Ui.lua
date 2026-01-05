7acca6ef64ddecfb68969dc2cffb006ae59b0af0
YZYLover <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2026-01-06 00:28:17 +0100

    Update Ui.lua

--- src/lib/Ui.lua
+++ src/lib/Ui.lua
@@ -74,7 +74,7 @@ type Log = {
 local SetClipboard = setclipboard or toclipboard or set_clipboard
 
 --// Libraries
-local ReGui = loadstring(game:HttpGet('https://github.com/x7dJJ9vnFH23/Dear-ReGui/raw/refs/heads/main/ReGui.lua'), "ReGui")()
+local ReGui = loadstring(game:Get('https://github.com/x7dJJ9vnFH23/Dear-ReGui/raw/refs/heads/main/ReGui.lua'), "ReGui")()
 
 --// Modules
 local Flags
@@ -559,7 +559,7 @@ function Ui:MakeOptionsTab(InfoSelector)
 			{
 				Text = "Copy Github",
 				Callback = function()
-					self:SetClipboard("https://github.com/depthso/Sigma-Spy")
+					self:SetClipboard("https://github.com/x7dJJ9vnFH23/Dear-ReGui")
 				end,
 			},
 			{
