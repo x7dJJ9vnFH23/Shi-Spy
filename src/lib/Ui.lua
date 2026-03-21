448395195ea360fda338c5a6b5b35a7a766972ca
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2026-03-22 00:25:30 +0100

    Update Ui.lua

--- src/lib/Ui.lua
+++ src/lib/Ui.lua
@@ -73,7 +73,7 @@ type Log = {
 local SetClipboard = setclipboard or toclipboard or set_clipboard
 
 --// Libraries
-local ReGui = loadstring(game:HttpGet('https://raw.githubusercontent.com/Awakenchan/Dear-ReGui/refs/heads/main/ReGui.lua'), "ReGui")()
+local ReGui = loadstring(game:HttpGet('https://raw.githubusercontent.com/hookop/regui/refs/heads/main/Main.luau'), "ReGui")()
 
 --// Modules
 local Flags
